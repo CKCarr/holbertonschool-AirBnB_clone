1f916f4e59253f92752eca6d8ab87a6d1a6172e7
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-06-09 09:51:10 -0700

    test for adding to_dictionary args kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -11,11 +11,18 @@ class BaseModel:
     """class BaseModel that defines all common
     attributes/methods for other classes:"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Public attribuites initializing base Models """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            if key == "created_at" or key == "updated_at":
+                value = datetime.strptime(value,
+                                                "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """return string with class name,
