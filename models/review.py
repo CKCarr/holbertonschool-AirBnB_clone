79af61b02ded90fecd43ff1237ccb69489658073
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-06-12 08:46:24 -0700

    models updated

---
+++ models/review.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+This module write the subclass Review
+"""
+
+from .base_model import BaseModel
+
+
+class Review(BaseModel):
+    """
+    The Review class is a subclass of BaseModel.
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+This module writes the subclass State.
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    The State class is a subclass of BaseModel.
+    """
+    name = ""
